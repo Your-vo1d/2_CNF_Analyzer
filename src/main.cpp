5051bb3901fea7fa85ea677b62c6c4e442e2a18f
Your_Vo1d <my.vo1d.edge@gmail.com>
2026-03-06 13:20:33 +0700

    Добавление отладочного вывода ошибки при передаче в качестве параметра пути файла

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <QCoreApplication>
+﻿#include <QCoreApplication>
+#include <QFileInfo>
 #include <iostream>
 #include "Parser_JSON.h"
 
@@ -6,12 +7,21 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    if (argc != 2) {
+    if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " <json_file_path>" << std::endl;
         return 1;
     }
 
+    // argv в Linux обычно
     QString file_path = QString::fromUtf8(argv[1]);
+
+    // Доп.диагностика, чтобы сразу видеть проблему с путём
+    QFileInfo fi(file_path);
+    if (!fi.exists() || !fi.isFile()) {
+        std::cout << "Error: file not found: " << file_path.toStdString() << std::endl;
+        std::cout << "Usage: " << argv[0] << " <json_file_path>" << std::endl;
+        return 1;
+    }
 
     Parser_JSON parser;
     bool success = parser.parseJsonFile(file_path);
@@ -30,6 +40,7 @@ int main(int argc, char *argv[])
         if (parser.hasError()) {
             std::cout << "Errors occurred during parsing." << std::endl;
         }
+        return 2;
     }
 
     return 0;
